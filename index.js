4e8bc95cc0c0ac957871f31ad00e808e754a8517
Ming Liu <vmliu1@gmail.com>
2017-07-31 21:37:51 -0700

    First PoC implementation

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 /* eslint-env node */
 'use strict';
 
+let MergeTrees = require('broccoli-merge-trees');
+let WriteSass = require('./lib/broccoli/sass-mixin');
+let stew = require('broccoli-stew');
+
 module.exports = {
-  name: 'ember-functional-css'
+  name: 'ember-functional-css',
+
+  treeForStyles() {
+    let tree = this._super(...arguments);
+
+    this.classTrees = {
+      self: new WriteSass(),
+      parent: new WriteSass()
+    };
+
+    return stew.log(new MergeTrees([].concat(tree || [], this.classTrees.self, this.classTrees.parent), { overwrite: true }));
+  },
+
+  setupPreprocessorRegistry(type, registry) {
+    registry.add('htmlbars-ast-plugin', {
+      name: 'class-mixin-extractor',
+      plugin: require('./lib/htmlbars-ast-plugin')(() => {
+        return this.classTrees[type];
+      }),
+      baseDir() {
+        return __dirname;
+      }
+    });
+  }
 };
