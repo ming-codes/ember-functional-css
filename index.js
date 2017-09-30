a473a038c3dad344dcd293dbb07a74b5c4d842c3
Ming Liu <vmliu1@gmail.com>
2017-09-30 00:43:47 -0700

    wip

--- index.js
+++ index.js
@@ -8,26 +8,32 @@ let stew = require('broccoli-stew');
 module.exports = {
   name: 'ember-functional-css',
 
-  treeForStyles() {
-    let tree = this._super(...arguments);
-
-    this.classTrees = {
-      self: new WriteSass(),
-      parent: new WriteSass()
-    };
-
-    return stew.log(new MergeTrees([].concat(tree || [], this.classTrees.self, this.classTrees.parent), { overwrite: true }));
+  preprocessTree(type, tree) {
+    if (type === 'css') {
+      // we actually can add to parent addon via addon-tree-output
+      return stew.log(tree, { label: 'pree css' });
+    }
+    return tree;
   },
 
-  setupPreprocessorRegistry(type, registry) {
-    registry.add('htmlbars-ast-plugin', {
-      name: 'class-mixin-extractor',
-      plugin: require('./lib/htmlbars-ast-plugin')(() => {
-        return this.classTrees[type];
-      }),
-      baseDir() {
-        return __dirname;
-      }
-    });
-  }
+  //treeForStyles() {
+  //  let tree = this._super(...arguments);
+
+  //  this.classTrees = {
+  //    self: new WriteSass(),
+  //    parent: new WriteSass()
+  //  };
+
+  //  return stew.log(new MergeTrees([].concat(tree || [], this.classTrees.self, this.classTrees.parent), { overwrite: true }));
+  //},
+
+  //setupPreprocessorRegistry(type, registry) {
+  //  registry.add('css', {
+  //    name: 'class-mixin-extractor',
+
+  //    toTree(tree) {
+  //      return stew.log(tree);
+  //    }
+  //  });
+  //}
 };
